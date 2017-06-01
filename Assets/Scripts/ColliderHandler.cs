b5baa355eab4576f93241456829c7c4120d821d4
Mikko <H9580@student.jamk.fi>
2017-06-01 17:01:12 +0300

    Scripts / Inventory

---
+++ Assets/Scripts/ColliderHandler.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColliderHandler : MonoBehaviour
+{
+    private Transform parent;
+    private GameObject parentGameObject;
+    private CanvasGroup inventory;
+    public static bool hitDetected;
+
+    void Start()
+    {
+        GameObject temp = GameObject.Find("Inventory");
+        inventory = temp.GetComponent<CanvasGroup>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchSupported == true || Input.GetMouseButton(0) == true)
+        {
+            CastRay();
+        }
+    }
+
+    void CastRay()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity);
+        if (hit && hit.collider != null && inventory.alpha == 0)
+        {
+            parent = hit.collider.gameObject.transform.parent;
+            parentGameObject = parent.gameObject;
+            Debug.Log(parentGameObject);
+            hitDetected = true;
+        }
+        else
+            hitDetected = false;
+    }
+}
---
+++ Assets/Scripts/GUIScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class GUIScript : MonoBehaviour {
+
+    public CanvasGroup inventory;
+    private string button;
+
+	// Use this for initialization
+	void Start ()
+    {
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.touchSupported == true || Input.GetMouseButton(0) == true)
+        {
+            if (ColliderHandler.hitDetected == true)
+            {
+                inventory.alpha = 1;
+            } 
+        }
+    }
+
+    public void ButtonClicked(GameObject rtClicked)
+    {
+        button = EventSystem.current.currentSelectedGameObject.name;
+
+        switch (button)
+        {
+            case "Exit":
+                inventory.alpha = 0;
+                break;
+        }
+    }
+}
