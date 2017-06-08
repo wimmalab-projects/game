01b0fd98d1c264118e617b037cd5bb29d295122b
Mikko <H9580@student.jamk.fi>
2017-06-08 18:09:03 +0300

    Added grape crush

---
+++ Assets/Scripts/CrushScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrushScript : MonoBehaviour
+{
+    GameObject[] spawnpoint;
+    public GameObject grape;
+    private GameObject temp;
+    private float startTime, refire = 0.3f;
+    // Use this for initialization
+    void Start()
+    {
+        startTime = Time.time;
+        spawnpoint = GameObject.FindGameObjectsWithTag("Respawn");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(startTime + refire <= Time.time)
+        {
+            temp = Instantiate(grape);
+            temp.transform.SetParent(gameObject.transform);
+            temp.transform.position = spawnpoint[Random.Range(0, spawnpoint.Length)].transform.position;
+            startTime = Time.time;
+        }
+    }
+}
---
+++ Assets/Scripts/grapeScript.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class grapeScript : MonoBehaviour
+{
+    private Vector2 mousePos;
+    private GameObject temp;
+    private Camera crushCamera;
+    public LayerMask layerMask;
+    // Use this for initialization
+    void Start()
+    {
+        temp = GameObject.Find("CrushCamera");
+        crushCamera = temp.GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = crushCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity, layerMask);
+            if (hit.collider.gameObject.tag == "Respawn")
+            {
+                Destroy(hit.collider.gameObject);
+            }
+        }
+    }
+}
